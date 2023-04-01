0bfdbfa1a041ec08e49b34b2ec494acfc1bde45b
Prakhar Varshney <70899048+PrakharV10@users.noreply.github.com>
2023-04-01 21:22:37 +0530

    chore: cleanup unused file

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-var indexRouter = require("./routes/index");
-var getEvents = require("./routes/get-events");
-var auth = require("./routes/auth");
-var token = require("./routes/token");
-var icalEvent = require("./routes/create-event-ical");
+var indexRouter = require('./routes/index');
+var icalEvent = require('./routes/create-event-ical');
 
 var app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/ical-event", icalEvent)
-app.use("/auth", auth);
-app.use("/get-events", getEvents);
-app.use("/token", token);
-app.use("/", indexRouter);
+app.use('/ical-event', icalEvent);
+app.use('/', indexRouter);
 
 var listener = app.listen(8080, function () {
-  console.log("Listening on port " + listener.address().port);
+	console.log('Listening on port ' + listener.address().port);
 });
